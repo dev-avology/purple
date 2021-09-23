76206d62a1bc00b2bc5e915058206bcc0ea75582
Manu Sharma <manusharma89880@gmail.com>
2021-09-23 17:53:48 +0530

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "${__dirname}/src/images/icon.png",
+        icon: `${__dirname}/src/images/icon.png`,
       },
     },
     "gatsby-plugin-sharp",
@@ -27,7 +27,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "${__dirname}/src/images/",
+        path: `${__dirname}/src/images/`,
       },
       __key: "images",
     },
