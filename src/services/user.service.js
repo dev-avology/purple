632407c1254faf69dd2272f45aeba8c93578b8bb
devavology <devavology12@gmail.com>
2022-04-25 11:02:42 +0530

    API URL Fix

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-
+const apiURL = "https://poojas.sg-host.com/purple/backend-services";
 // Wiil be removed
 const getUserData = () => {
-  return axios.get(process.env.REACT_APP_API_URL+"/api/get-user-data", { mode: 'no-cors', headers: authHeader() });
+  return axios.get(apiURL+"/api/get-user-data", { mode: 'no-cors', headers: authHeader() });
 };
 
 const getDesignCount = () => {
-  return axios.get(process.env.REACT_APP_API_URL+"/api/get-designs-count", { mode: 'no-cors', headers: authHeader() });
+  return axios.get(apiURL+"/api/get-designs-count", { mode: 'no-cors', headers: authHeader() });
 };
 
 const getFeaturedProducts = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/get-featured-products");
+  return axios.get(apiURL + "/api/get-featured-products");
 };
 
 const getAllCategories = () => {
@@ -19,66 +19,66 @@ const getAllCategories = () => {
 };
 
 const UpdateProfileData = (data) => {
-  return axios.post(process.env.REACT_APP_API_URL + "/api/save-user-profile", data, {headers: authHeader()});
+  return axios.post(apiURL + "/api/save-user-profile", data, {headers: authHeader()});
 }
 
 const ChangePassword = (data) => {
-  return axios.post(process.env.REACT_APP_API_URL + "/api/change-password", data, {headers: authHeader()});
+  return axios.post(apiURL + "/api/change-password", data, {headers: authHeader()});
 }
 
 const getArtworkMedia = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/get-artwork-media", { headers: authHeader() });
+  return axios.get(apiURL + "/api/get-artwork-media", { headers: authHeader() });
 }
 
 const saveWishlist = (data) => {
-  return axios.post(process.env.REACT_APP_API_URL + "/api/save-wishlist", data, { headers: authHeader() });
+  return axios.post(apiURL + "/api/save-wishlist", data, { headers: authHeader() });
 }
 
 const getProductDetail = async () => {
-  const response = await axios.get(process.env.REACT_APP_API_URL+'/api/get-single-product', {params: data}, { headers: authHeader() })
+  const response = await axios.get(apiURL+'/api/get-single-product', {params: data}, { headers: authHeader() })
     const data = await response.json()
     console.log(data)
       return data
     }
 
 const getCart = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/get-cart?is_wishlist_product=0", {headers: authHeader()});
+  return axios.get(apiURL + "/api/get-cart?is_wishlist_product=0", {headers: authHeader()});
 }
 
 const getWishlist = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/get-cart?is_wishlist_product=1", {headers: authHeader()});
+  return axios.get(apiURL + "/api/get-cart?is_wishlist_product=1", {headers: authHeader()});
 }
 
 const removeFromCart = (params) => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/remove-from-cart", {params: params, headers: authHeader()});
+  return axios.get(apiURL + "/api/remove-from-cart", {params: params, headers: authHeader()});
 }
 
 const DecrementCart = (params) => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/decrement-cart", {params: params, headers: authHeader()});
+  return axios.get(apiURL + "/api/decrement-cart", {params: params, headers: authHeader()});
 }
 
 const IncrementCart = (params) => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/increment-cart", {params: params, headers: authHeader()});
+  return axios.get(apiURL + "/api/increment-cart", {params: params, headers: authHeader()});
 }
 
 const removeFromWishlist = (params) => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/remove-from-wishlist", {params: params, headers: authHeader()});
+  return axios.get(apiURL + "/api/remove-from-wishlist", {params: params, headers: authHeader()});
 }
 
 const saveArtwork = (data) => {
-  return axios.post(process.env.REACT_APP_API_URL + "/api/save-art-work", data, { headers: authHeader() });
+  return axios.post(apiURL + "/api/save-art-work", data, { headers: authHeader() });
 }
 
 const addToCart = (data) => {
-  return axios.post(process.env.REACT_APP_API_URL + "/api/save-cart", data, { headers: authHeader() });
+  return axios.post(apiURL + "/api/save-cart", data, { headers: authHeader() });
 }
 
 const getExploreDesign = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/get-explore-design");
+  return axios.get(apiURL + "/api/get-explore-design");
 };
 
 const getFanartMadeByArtist = () => {
-  return axios.get(process.env.REACT_APP_API_URL + "/api/fanart-madeby-artist");
+  return axios.get(apiURL + "/api/fanart-madeby-artist");
 };
 
 const UserService = {
